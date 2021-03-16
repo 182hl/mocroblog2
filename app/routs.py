57428bf1aabd53098a4776733066db40b676e781
182hl <2550886861@qq.com>
2021-03-16 22:53:18 +0800

    commit

--- app/routs.py
+++ app/routs.py
@@ -1,4 +1,5 @@
 from app import app
+from flask import render_template
 
 #2个路由
 @app.route('/')
@@ -6,4 +7,22 @@ from app import app
 
 #1个视图函数
 def index():
-    return "hello,World" #返回一个字符串
+    user = {"username": "Migeal"}
+    # return "hello,World" #返回一个字符串
+
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html',user=user,posts=posts)
+
+
+
+
+
